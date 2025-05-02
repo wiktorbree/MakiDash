82b72dd1315de0671adf9284345dc58fbc08396c
Wiktor <bramerwiktor@gmail.com>
2025-05-02 23:19:51 +0200

    Initial Commit

---
+++ MakiDash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MakiDash
+//
+//  Created by Wiktor Bramer on 02/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MakiDash/MakiDashApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakiDashApp.swift
+//  MakiDash
+//
+//  Created by Wiktor Bramer on 02/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct MakiDashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
